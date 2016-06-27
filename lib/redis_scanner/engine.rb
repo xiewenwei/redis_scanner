ff011b915ded9894fd155ce653f846e106273668
vincent <vincent@boohee.com>
2016-06-27 20:03:53 +0800

    Add progressbar for scanning.

--- lib/redis_scanner/engine.rb
+++ lib/redis_scanner/engine.rb
@@ -1,3 +1,5 @@
+require "ruby-progressbar"
+
 module RedisScanner
   class Engine
     def initialize(redis, options)
@@ -21,9 +23,32 @@ module RedisScanner
       end
     end
 
+    def total_keys
+      ret = 0
+
+      if (info = @redis.info) && (str = info["db#{@options[:db].to_i}"])
+        if m = str.scan(/keys=(\d+)/)
+          ret = m.flatten.first.to_i
+        end
+      end
+
+      ret
+    end
+
+    def create_progress_bar
+      ProgressBar.create(
+        title: "Keys",
+        format: '%a %bᗧ%i %p%% %t',
+        progress_mark: ' ',
+        remainder_mark: '･',
+        total: total_keys)
+    end
+
     def scan
       cursor = 0
       stat = Hash.new(0)
+
+      bar = create_progress_bar
       while true
         if @options[:match]
           cursor, result = @redis.scan cursor, match: @options[:match]
@@ -33,10 +58,13 @@ module RedisScanner
         result.each do |key|
           pattern = resolve_pattern(key)
           stat[pattern] += 1
+          bar.increment
         end
         cursor = cursor.to_i
         break if cursor == 0
       end
+      bar.finish
+
       stat
     end
 
